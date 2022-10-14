b44babf559d26a81cea0039e10e0dcfb939de8e7
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-10-14 14:41:27 +0100

    made tightly coupled EmployeeBL class which depends on the EmployeeDAL class.

---
+++ DependencyInjectionExample/EmployeeBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionExample
+{
+    public class EmployeeBL
+    {
+        public EmployeeDAL employeeDal;
+
+        public List<Employee> GetAllEmployees()
+        {
+            employeeDal = new EmployeeDAL();
+            return employeeDal.SelectAllEmployees();
+        }
+    }
+}
---
+++ DependencyInjectionExample/EmployeeDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionExample
+{
+    public class EmployeeDAL
+    {
+        public List<Employee> SelectAllEmployees()
+        {
+            List<Employee> ListEmployees = new List<Employee>();
+
+            ListEmployees.Add(new Employee(){ID = 1, Name = "Pranaya", Department = "IT"});
+            ListEmployees.Add(new Employee(){ID = 2, Name = "Kumar", Department = "HR"});
+            ListEmployees.Add(new Employee(){ID = 3, Name = "Sheila", Department = "ELT"});
+            return ListEmployees;
+        }
+    }
+}
